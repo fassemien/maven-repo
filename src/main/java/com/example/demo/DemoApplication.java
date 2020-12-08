a729972977c37352e117cfbf27fe6229f4cc3fd0
Fabrice ASSEMIEN <asskroah@gmail.com>
2020-12-08 18:31:08 +0000

    test2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	public String getName(){
+		return "Assemien Fabrice";
+	}
 }
